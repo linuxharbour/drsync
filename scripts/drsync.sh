db7b3c3ea24551b100de25c6aa349131632c8645
Sammy Fung <sammy@sammy.hk>
2016-03-23 18:37:05 +0800

    added custom SSH port support

--- scripts/drsync.sh
+++ scripts/drsync.sh
@@ -21,9 +21,14 @@ do
   lab=`echo ${task} | cut -f 1 -d ,`
   src=`echo ${task} | cut -f 2 -d ,`
   dst=`echo ${task} | cut -f 3 -d ,`
+  sshport=`echo ${task} | cut -f 4 -d ,`
+  if [ a${sshport} == 'a' ];
+  then
+    sshport=22
+  fi
   start=`date`
   echo Rsync Started at ${start} >> ${logfile}-${lab}.log
-  rsync -av -e "ssh" ${src} ${dst} | tee -a ${logfile}-${lab}.log 
+  rsync -av -e "ssh -p ${sshport}" ${src} ${dst} | tee -a ${logfile}-${lab}.log 
   end=`date`
   echo Rsync Ended at ${end} >> ${logfile}-${lab}.log
   rstat=`tail -n 4 ${logfile}-${lab}.log | head -n 2 | sed -e "s/[\r\n]//g" -e "s/sent //g" -e "s/bytes //g" -e "s/received //g" -e "s/total //g" -e "s/speedup //g" -e "s/is //g" -e "s/bytes\/sec//g" -e "s/size //g" -e "s/,//g"`
